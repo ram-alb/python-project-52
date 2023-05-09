d6d86c428c1cfc9f3d7650abaaff50a538d9dd93
Ramil Albakov <ramil.albakov2@gmail.com>
2023-05-09 18:32:29 +0600

    Change DEBUG settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'true').lower() in {'yes', '1', 'true'}
 
 ALLOWED_HOSTS = [
     'webserver',
